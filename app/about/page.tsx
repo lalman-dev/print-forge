c23f890b23b87054810e3434e96a8db08499a3d8
Lalman <lalmanchaudhary2@gmail.com>
2025-12-25 20:46:29 +0530

    feat: initialize Next.js app , delete unnecessary files and folders and add homepage layout

---
+++ app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function AboutPage() {
+  return <h1 className="text-cyan-500">This is About page</h1>;
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,3 @@
+export default function Products (){
+  return <h1 className="text-9xl text-purple-500">this is Products page with multiple pages inti </h1>
+}
